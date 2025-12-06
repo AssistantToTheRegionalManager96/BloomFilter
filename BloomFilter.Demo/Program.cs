a67dfc2503176c45f1be4e9b997ed9f500ff2d2f
Michal Urbanski <michalurbanski960@gmail.com>
2025-12-06 13:24:01 +0000

    split projects

---
+++ BloomFilter.Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using BloomFilter;
+using Murmur;
+using System;
+using System.Text;
+using BloomFilter;
+
+var words = new List<string>
+{
+    "cat", "shit", "weed", "chronic", "bubonic", "babayaga", "Luigi Mangione", "booba"
+};
+
+var words2 = new List<string>
+{
+    "bitch", "work", "Coriolanus", "boombox", "Stankonia"
+};
+
+var filter = new BloomFilter.BloomFilter(1000, 3);
+
+foreach (var word in words) filter.AddWord(word);
+
+var present = true;
+foreach (var word in words) present = present & filter.CheckWord(word);
+
+var g = words2.Select(x => filter.CheckWord(x)).ToList();
+var a = "a";
+
+
+
+
+
--- BloomFilter/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Murmur;
-using System;
-using System.Text;
-
-var word1 = "cat";
-var word2 = "shit";
-
-
-
